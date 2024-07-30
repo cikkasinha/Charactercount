b58b9e43c16d406588670e4549bbf32b507a74ad
cikkasinha <111222480+cikkasinha@users.noreply.github.com>
2024-07-30 20:28:54 +0530

    Update CharacterCount.ps1

--- CharacterCount.ps1
+++ CharacterCount.ps1
@@ -1,3 +1,11 @@
+<#
+.SYNOPSIS
+    This PowerShell script prompts the user to enter a server name through an input box and then displays the character count of the server name in a message box. It utilizes .NET assemblies to create user-friendly graphical interfaces for input and output, enhancing usability and interaction.
+
+.AUTHOR
+    Chandrakant Sinha
+#>
+
 # Function to show an input box
 function Show-InputBox {
     param (
